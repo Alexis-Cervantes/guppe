fe2634adf469130db69762fddc6b7ef474724c69
Alexis Cervantes <iacervantes@outlook.com>
2024-02-06 12:33:03 -0300

    Entendendo o pycharm

---
+++ dir_e_help.py
@@ -0,0 +1,26 @@
+"""
+Utilitarios Python para te auxiliar na programação
+
+dir: Apresenta todos os atributos/propriedades e funções/metodos disponiveos para determinado tipo de dado o variavel
+exe: dir(tipo de dado o vaiavel)
+     dir("Alexis") ENTER
+     ['__add__', '__class__', '__contains__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__getnewar
+gs__', '__getstate__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mod__', '__mul__', '__ne__', '__ne
+w__', '__reduce__', '__reduce_ex__', '__repr__', '__rmod__', '__rmul__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', 'capitalize', 'casefold',
+ 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isi
+dentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'removeprefi
+x', 'removesuffix', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title'
+, 'translate', 'upper', 'zfill']
+
+
+help: Apresenta a documentação/ como utilizar atributos/propriedades e funções/metodos disponiveis para detreminado
+      tipo e dado ou variavel
+      Exe:
+       help("Alexis".lower)
+       Help on built-in function lower:
+
+       lower() method of builtins.str instance
+       Return a copy of the string converted to lowercase.
+
+"""
+print('Alexis')
