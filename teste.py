199d6fad9108d3f4e5b929d4588a05ad36e9d0dc
Alexis Cervantes <iacervantes@outlook.com>
2024-02-12 18:33:21 -0300

    Atualizando...

--- teste.py
+++ teste.py
@@ -1,2 +1,36 @@
-nome = "Alexis"
-print(nome)
+vendedores = ['Marcus', 'Amanda', 'Ale', 'Karol']
+vendas = [15, 10, 20, 30]
+
+# Iteração 01:
+for vendedor in vendedores:
+    print(vendedor)
+
+print('*****************')
+
+# Iteração 02: RANGE 1:
+'''Temos que usar uma lista de indices "tamanho_lista" para que seja iteravel em Range'''
+tamanho_lista = len(vendedores)
+for i in range(tamanho_lista):
+    print(vendedores[i])
+
+print('*****************')
+
+# Iteração 02: RANGE 2:
+tamanho_lista = len(vendedores)
+for i in range(tamanho_lista):
+    print(vendedores[i])
+    print(vendas[i])
+
+print('*****************')
+
+# Iteração 03: ENUMERATE:
+for i, vendedor in enumerate(vendedores):
+    print(vendedor)
+    print(vendas[i])
+
+print('*****************')
+
+# Iteração 04: ZIP:
+for vendedor, venda in zip(vendedores, vendas):
+    print(vendedor)
+    print(venda)
