64a30d89602aa1128fa50c4260ab64ca4def9d84
Alexis Cervantes <iacervantes@outlook.com>
2024-03-15 18:25:55 -0300

    Acabando Filter...

--- filter.py
+++ filter.py
@@ -41,12 +41,43 @@ paises = ['', 'Argentina', '', 'Brasil', 'Chile', '', 'Colombia', '', 'Equador',
 # print(f'paises sem filter: {paises}')
 
 res2 = filter(None, paises)  # None elimina espaçoes em branco
-print(f'paises com filter {list(res2)}')
+# print(f'paises com filter {list(res2)}')
 
 # Outro modo de fazer a mesma coisa que o FILTER utilizando LAMBDA 01
 res3 = filter(lambda pais: len(pais) > 0, paises)
-print(f'paises utlizando função lambda v1: {list(res3)}')
+# print(f'paises utlizando função lambda v1: {list(res3)}')
 
 # Outro modo de fazer a mesma coisa que o FILTER utilizando LAMBDA 02
 res4 = filter(lambda pais: pais != '', paises)
-print(f'paises utlizando função lambda v2: {list(res4)}')
+# print(f'paises utlizando função lambda v2: {list(res4)}')
+
+'''Exemplos mais complexos'''
+# Você foi contratado pelo 'Twitter" para analizar os twtters de um usuarios:
+usuarios = [
+    {'Username': 'Samuel', 'tweets': ['Eu adoro bolos', 'Eu adoro pizzas']},
+    {'Username': 'carla', 'tweets': ['Eu amo meu gato']},
+    {'Username': 'Jeff', 'tweets': []},
+    {'Username': 'bob123', 'tweets': []},
+    {'Username': 'doggo', 'tweets': ['Eu gosto de cachorros', 'Vou sair hoje']},
+    {'Username': 'gal', 'tweets': []},
+]
+# TAREFA: Filtrar os usuarios que estão inativos
+# print(usuarios)
+
+# Forma 1:
+inativos1 = list(filter(lambda usuario: len(usuario['tweets']) == 0, usuarios))
+# print(inativos1)
+
+# Forma 2: REFATORANDO
+inativos2 = list(filter(lambda usuario: not usuario['tweets'], usuarios))
+# print(inativos2)
+
+'''COMBINANDO FILTER E MAP'''
+nomes = ['Vanessa', 'Ana', 'Maria']
+
+# TAREFA: Devemos criar uma lista contendo testo 'sua insttrutora é + nome'; desde que cada nome tenha menos de 5 carac.
+
+lista_instrutora = list(map(lambda nome: f'Sua instrutora é {nome}', filter(lambda nome: len(nome) < 5, nomes)))
+# print(lista_instrutora)
+
+print('Olá você está no arquivo Filter()')
