d985d0a5f45a23e81f62df3c1fb923c41606fc1f
Alexis Cervantes <iacervantes@outlook.com>
2024-04-16 19:14:23 -0300

    Acabando modulos customizados.

---
+++ 12-Trabalhando_com_modulos_e_pacotes_py/modulos_customizados.py
@@ -0,0 +1,26 @@
+"""Modulos Customizados"""
+'''
+Como modulos pythom nada mais são do que arquivos Python, ENTÂO todos os arquivos que criamos neste curso são modulos
+Python prontos para serem utilizados:
+'''
+# Importando uma função especifica do nosso modulo. Vamos usar como exemplo um arquivo python existente nesta pastas
+# from funcoes_com_parametro2 import soma_impares
+
+# print(soma_impares([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+
+# Importando thodo o modulo (Temos acesso ao thodo o modulo). Vamos usar como exemplo um arquivo python existente nesta
+# pastas
+# import funcoes_com_parametro2 as fcp
+
+# print(fcp.lista)  # Estamos acesando e imprimindo uma variavel contida no modulo
+# print(fcp.tupla)
+# print(fcp.soma_impares(fcp.lista))
+
+# Importando 02 funções especificas do modulo map2. Vamos usar como exemplo um arquivo python existente nesta pastas
+# from map2 import cidades, c_para_f
+# print(list(map(c_para_f, cidades)))
+
+print('Olá esta no arquivo modulos customizados')
+
+
+
