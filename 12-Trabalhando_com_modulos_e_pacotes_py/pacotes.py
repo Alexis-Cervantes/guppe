a4c3ead7e0aca296a08e075a62e7eb62831fd9d6
Alexis Cervantes <iacervantes@outlook.com>
2024-04-19 12:50:39 -0300

    Iniciando pacotes

---
+++ 12-Trabalhando_com_modulos_e_pacotes_py/pacotes.py
@@ -0,0 +1,11 @@
+"""Pacotes"""
+
+'''
+Módulo -> É apenas um arquivo Python que podem ter diversas funções para utilizarmos
+Pacote -> É um diretorio contendo uma coleção de modulos.
+
+OBS: Nas versões 2.x do python, um pacote python deveria conter dentro dele um arquivo chamado __init__.py
+Nas versões 3.x do python, não é mais obrigatorio a utilização deste arquivo, mas, normalmente ainda é utilizado para
+manter compatibiliadde  
+
+'''
