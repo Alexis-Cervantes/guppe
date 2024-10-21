b11895902891f150492ffa012433e0ad5f731aca
Alexis Cervantes <iacervantes@outlook.com>
2024-10-21 19:59:42 -0300

    Iniciando iterador customizado

---
+++ 14-Iteradores_e_geradores/iterador_customizado.py
@@ -0,0 +1,14 @@
+"""
+Escrevendo um Iterador Customizado
+"""
+# Vammos fazer um iterador parecido a do que faz um for/range
+# for n in range(0, 11):
+#     print(n)
+
+class Contador:
+    def __init__(self, menor, Maior):
+        self.menor = menor
+        self.Maior = Maior
+
+
+
