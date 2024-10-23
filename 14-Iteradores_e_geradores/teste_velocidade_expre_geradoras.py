5b9e26a0ac9c5ac5bf4f401e824a14e6a1c20d37
Alexis Cervantes <iacervantes@outlook.com>
2024-10-23 18:00:02 -0300

    Acabando teste_velocidade_expre_geradoras

--- 14-Iteradores_e_geradores/teste_velocidade_expre_geradoras.py
+++ 14-Iteradores_e_geradores/teste_velocidade_expre_geradoras.py
@@ -2,8 +2,6 @@
 Teste de velocidade com expressões geradoras
 """
 from types import new_class
-
-
 # Diferença:
 
 # GENERATOR:
@@ -27,5 +25,19 @@ from types import new_class
 # print(next(ge2))
 # print(next(ge2))
 
-# Exemplos
-print(sum(num for num in range(1, 4)))
+# Outros Exemplos de teste de velocidade:
+# import time
+
+# gen_inicio = time.time()
+# print(sum(num for num in range(10000))) # 1000  -> 0.0011157989501953125 - MAIS EFICIENTE
+# gen_tempo = time.time() - gen_inicio
+
+# List Comprehension
+# list_inicio = time.time()
+# print(sum([num for num in range(10000)])) # 1000 -> 0.0011441707611083984 - ESTUVO CERCA DE LA OTRA
+# list_tempo = time.time() - list_inicio
+
+# print(f'Generators Expression levou: {gen_tempo}')
+# print(f'List Comprehension levou: {list_tempo} ')
+
+print('teste de velocidade com expreções geradoras')
