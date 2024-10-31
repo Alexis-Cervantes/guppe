483a30af5c4d55718f986b58acaf5d7938af401c
Alexis Cervantes <iacervantes@outlook.com>
2024-10-31 20:02:38 -0300

    continuando metodos

--- 16-POO-OPP/metodos.py
+++ 16-POO-OPP/metodos.py
@@ -8,7 +8,9 @@ partir da classe.
 - Os métodos/funções dunder em pythom são chamados de métodos mágicos
 - Não é recomendado criar métodos/funções com duplo underline. Porque pythom cria seus me´todos desse jeito. Seu
 codigo pode dar errado
+- métodos se emcrevem com letra minuscula, e se são composto se coloca um aunderline entres ambas.
 """
+from idlelib.pyshell import use_subprocess
 
 """MÉTODOS DE INSTÂNCIA"""
 class Lampada:
@@ -38,12 +40,64 @@ class Produto:
         self.__nome = nome
         self.__descricao = descricao
         self.__valor = valor
+        Produto.contador = self.__id
 
+    def desconto(self, porcentagem):    # méthodo
+        """Retorna o valor do produto com o desconto"""
+        return (self.__valor * (100 - porcentagem)) / 100
+
+from passlib.hash import pbkdf2_sha256 as cryp
 class Usuario:
 
-    def __init__(self, nome, email, senha):
+    def __init__(self, nome, sobrenome, email, senha):
         self.__nome = nome
+        self.__sobrenome = sobrenome
         self.__email = email
-        self.__senha = senha
+        self.__senha = cryp.encrypt(senha, rounds=200000, salt_size=16)
 
+    def nome_completo(self):    # méthodo
+        return f'{self.__nome} {self.__sobrenome}'
 
+    def checa_senha(self, senha):
+        if cryp.verify(senha, self.__senha):
+            return True
+        return False
+
+# Criando produto ou intanciando a clase:
+# p1 = Produto('Play Station 4', 'Video Game', 2300)
+# print(p1.desconto(20))
+# OU
+# print(Produto.desconto(p1, 20)) # passamos o self, desconto
+
+# Instanciando Usuatio:
+user1 = Usuario('Alexis', 'Cervantes', 'iacervantes@outlook.com', '123456')
+user2 = Usuario('Eduarda', 'Cervantes', 'madudacervantes@gmail.com', '123456')
+
+print(user1.nome_completo())
+print(user2.nome_completo())
+
+# Acessos errados:
+print(f'Senha user1: {user1._Usuario__senha}')
+print(f'Senha user2: {user2._Usuario__senha}')
+
+# Vamos a baixar uma biblioteca chamada de 'passlib' no cabeçalho da clase para ocultar a senha
+nome = input('Informe o nome: ')
+sobrenome = input('Informe o sobrenome: ')
+email = input('Informe o email: ')
+senha = input('Informe a senha; ')
+confirma_senha = input('Confirme a senha: ')
+
+if senha == confirma_senha:
+    user = Usuario(nome, sobrenome, email, senha)
+else:
+    print('senha não confere')
+
+print('Usuario criado com sucesso')
+senha = input('Informe a senha para acesso: ')
+
+if user.checa_senha(senha):
+    print('Acessos permitido')
+else:
+    print('Acesso negado')
+
+print(f'Senha User Criptografiada: {user._Usuario__senha}')
