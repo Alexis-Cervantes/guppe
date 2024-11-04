d7c5829ee4904169ab6eafddb870389123d9b53b
Alexis Cervantes <iacervantes@outlook.com>
2024-11-04 20:07:37 -0300

    acabando objetos.py

--- 16-POO-OPP/objetos.py
+++ 16-POO-OPP/objetos.py
@@ -4,42 +4,68 @@ POO - Objetos
 devemos poder criar quantos objetos forem necesarios. Podemos pensar nos objetos/instâncias de uma clase como variaveis
 do tipo definido na clase.
 """
-class Lampada:
+# class Lampada:
+#
+#     def __init__(self, cor, voltagem, luminosidade):
+#         self.__cor = cor
+#         self.__voltagem = voltagem
+#         self.__luminosidade = luminosidade
+#         self.__ligada = False
 
-    def __init__(self, cor, voltagem, luminosidade):
-        self.cor = cor
-        self.voltagem = voltagem
-        self.luminosidade = luminosidade
-        self.ligada = False
+    # def checa_lampada(self):
+    #     return self.__ligada
 
-    def checa_lampada(self):
-        return self.ligada
+    # def ligar_desligar(self):
+    #     if self.__ligada:
+    #         self.__ligada = False
+    #     else:
+    #         self.__ligada = True
 
-    def liga_desligar(self):
-        if
+# class Cliente:
+#     def __init__(self, nome, cpf):
+#         self.__nome = nome
+#         self.__cpf = cpf
 
-class ContaCorrente:
+    # def diz(self):
+    #     print(f'O cliente é {self.__nome} diz oi')
 
-    contador = 4999
+# class ContaCorrente:
 
-    def __init__(self, limite, saldo):
-        self.numero = ContaCorrente.contador + 1
-        self.limite = limite
-        self.saldo = saldo
-        ContaCorrente.contador = self.__numero
+    # contador = 4999
 
-class Usuario:
+    # def __init__(self, limite, saldo, cliente):
+    #     self.__numero = ContaCorrente.contador + 1
+    #     self.__limite = limite
+    #     self.__saldo = saldo
+    #     self.__cliente = cliente
+    #     ContaCorrente.contador = self.__numero
 
-    def __init__(self, nome, sobrenome, email, senha):
-        self.nome = nome
-        self.sobrenome = sobrenome
-        self.email = email
-        self.senha = senha
+    # def mostra_cliente(self):
+    #     print(f'O cliente é {self.__cliente._Cliente__nome}')
+
+# class Usuario:
+
+    # def __init__(self, nome, sobrenome, email, senha):
+    #     self.__nome = nome
+    #     self.__sobrenome = sobrenome
+    #     self.__email = email
+    #     self.__senha = senha
 
 # Instancias/objetos:
-lamp1 = Lampada('branca', 110, 60)
-cc1 = ContaCorrente(5000, 20000)
-user1 = Usuario('Alexis', 'Cervantes', 'iacervantes@outlook.com', '123456')
+# lamp1 = Lampada('branca', 110, 60)  # estado inicial da lampada (deslifada = False
+# lamp1.ligar_desligar()  # Com esto ligamos a lampada
 
+# print(f'A lampada esta ligada? : {lamp1.checa_lampada()}')  # Saida-> True ou false
 
+# cc1 = ContaCorrente(5000, 20000)
+# user1 = Usuario('Alexis', 'Cervantes', 'iacervantes@outlook.com', '123456')
 
+# testando ContaCorrente e Cliente:
+# cli1 = Cliente('Alexis', '123.456.789-10')
+# cc = ContaCorrente(5000, 10000, cli1)
+# print(cc.mostra_cliente())
+
+# cc.mostra_cliente()
+# cc._ContaCorrente__cliente.diz()    # Aceeso de forma errada
+
+print('objetos')
