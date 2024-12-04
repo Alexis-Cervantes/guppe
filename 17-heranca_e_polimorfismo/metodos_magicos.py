1bfa195f5d2e3598188a50fe0848bd5052420d35
Alexis Cervantes <iacervantes@outlook.com>
2024-12-04 12:23:54 -0300

    métodos mágicos.

--- 17-heranca_e_polimorfismo/metodos_magicos.py
+++ 17-heranca_e_polimorfismo/metodos_magicos.py
@@ -23,9 +23,34 @@ class Livro:
     def __str__(self):
         return self.titulo
 
+    def __len__(self):
+        return self.paginas
 
-livro1 = Livro('Python Rocks', 'Alexis Cervantes', '400')
-livro2 = Livro('Inteligência Articial com Pythom', 'Eduarda Cervantes', '350')
+    def __del__(self):
+        print('um objeto de tipo Livro foi deletado da Memória')
 
-print(livro1)
-print(livro2)
+    def __add__(self, other):
+        return f'{self} - {other}'
+
+    def __mul__(self, other):
+        if isinstance(other, int):
+            msg = ' '
+            for n in range(other):
+                msg += ' ' + str(self)
+            return msg
+        return 'Não posso multiplicar'
+
+# livro1 = Livro('Python Rocks!', 'Alexis Cervantes', 400)
+# livro2 = Livro('Inteligência Articial com Pythom', 'Eduarda Cervantes', 350)
+
+# print(livro1)
+# print(livro2)
+
+# print(len(livro1))
+# print(len(livro2))
+
+# print(livro1 + livro2)
+
+# print(livro1 * 5)
+
+print('métodos mágicos')
