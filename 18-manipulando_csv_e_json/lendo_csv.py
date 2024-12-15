1f35b19134466bd80ee602a24451b6212f39d77a
Alexis Cervantes <iacervantes@outlook.com>
2024-12-15 13:35:49 -0300

    lendo arquivos csv

--- 18-manipulando_csv_e_json/lendo_csv.py
+++ 18-manipulando_csv_e_json/lendo_csv.py
@@ -3,12 +3,12 @@
 CSV -> Comma Separeted Values (Valores separados por virgula)'''
 
 Seaparador por virgula:
-EXE
+Exe
 1, 2, 3, 4, 5
 Maria, Alexis, Cacia, Eduarda
 
 Separador por ponto e virgula:
-EXE:
+Exe:
 1; 2; 3; 4; 5
 Maria; Alexis; Cacia; Eduarda
 
@@ -18,7 +18,47 @@ Maria Alexis Cacia Eduarda
 
 http://dados.gov.br/dataset
 """
-with open('lutadores.csv') as arquivo:
-    dados = arquivo.read()
-    print(type(dados))
-    print(dados)
+# # Muito trabalhosso: leitura de arquivos CSV
+# with open('lutadores.csv', encoding='utf8') as arquivo:
+#     dados = arquivo.read()
+#     # print(type(dados))
+#     # Limpeza de Dados: (.split: para separar as informações com virgula /  Slice: para comezar da segunda linha
+#     dados = dados.split(',')[2:]
+#     print(dados)
+
+# Forma Certa para ler arqivos CSV: Existem dois modos:
+"""
+- Reader: Permite itereção sobre as linhas do arquivo csv como listas;
+- DicReader: Permite ieteração sobre as linhas do arquivo csv como OrderedDicst;
+"""
+# Reader: Importando biblioteca:
+from csv import reader
+
+# with open('lutadores.csv', encoding='utf8') as arquivo:
+#     leitor_csv = reader(arquivo)
+#     next(leitor_csv) # Pular o cabeçalho
+#     for linha in leitor_csv:
+#         # OBS: cada linha é uma lista:
+#         print(f'{linha[0]}, nasceu no(a)(s) {linha[1]} e mede {linha[2]} centimetros')
+
+
+# DictReader: Importando biblioteca:
+from csv import DictReader
+# OBS: DictReader tem como padrão trabalhar com a (,) como delimitador
+
+# with open('lutadores.csv', encoding='utf8') as arquivo:
+#     leitor_csv = DictReader(arquivo)
+#     for linha in leitor_csv:
+#         # OBS: cada linha é uma Ordered Dict:
+#         print(f"{linha['Nome']}, nasceu no(a)(s) {linha['País']} e mede {linha['Altura (em cm)']}")
+
+
+# DictReader: Com outro delimitador(exe. ';', '/', '\' etc)
+
+# with open('lutadores.csv', encoding='utf8') as arquivo:
+#     leitor_csv = DictReader(arquivo, delimiter=',')
+#     for linha in leitor_csv:
+#         # OBS: cada linha é uma Ordered Dict:
+#         print(f"{linha['Nome']}, nasceu no(a)(s) {linha['País']} e mede {linha['Altura (em cm)']}")
+
+print('lendo arquivo csv')
