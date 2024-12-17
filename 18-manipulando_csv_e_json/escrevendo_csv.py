e8da5f48cee3e37ef890f8ddc4aee40d126acff6
Alexis Cervantes <iacervantes@outlook.com>
2024-12-17 11:40:32 -0300

    escrevendo_csv

--- 18-manipulando_csv_e_json/escrevendo_csv.py
+++ 18-manipulando_csv_e_json/escrevendo_csv.py
@@ -7,17 +7,39 @@ writerow() -> Escreve uma linha
 # writer(): gera um objeto para que possamos escrever em um arquivo csv.
 # writerow(): utiizado as linhas do arquivo .csv criado anteriormente. Ele receve uma lista
 
+# Trabalhando com WRITER
 from csv import writer
 
-with open('filmes.csv', 'w') as arquivo:
-    escritor_csv = writer(arquivo)
-    filme = None
-    escritor_csv.writerow(['Titulo', 'Gênero', 'Duração'])
-    while filme != 'sair':
-        filme = input('Informe o nome do filme: ')
-        if filme != 'sair':
-            genero = input('Informe o genero: ')
-            duracao = input('Informe a duaração (em minutos): ')
-            escritor_csv.writerow([filme, genero, duracao])
+# Se queremos acrecentar mas elementos usamos 'a' em vez de 'w'
+# with open('filmes.csv', 'w', encoding='utf8') as arquivo:
+#     escritor_csv = writer(arquivo)
+#     filme = None
+#     escritor_csv.writerow(['TÍtulo', 'Gênero', 'Duração'])
+#     while filme != 'sair':
+#         filme = input('Informe o nome do filme: ')
+#         if filme != 'sair':
+#             genero = input('Informe o Gênero: ')
+#             duracao = input('Informe a Duração (em minutos): ')
+#             escritor_csv.writerow([filme, genero, duracao])
+
+# Trabalhando com DictWriter
+from csv import DictWriter
+from pickletools import stringnl_noescape_pair
+
+# with open('filmes2.csv', 'w', encoding='utf8') as arquivo:
+#     cabecalho = ['Título', 'Gênero', 'Duração']
+#     escritor_csv = DictWriter(arquivo, fieldnames=cabecalho)
+#     escritor_csv.writeheader()
+#     filme = None
+#     while filme != 'sair':
+#         filme = input('Informe o nome do Filme: ')
+#         if filme != 'sair':
+#             genero = input('Informe o Gênero: ')
+#             duracao = input('Informe a Duração (em minutos): ')
+#             escritor_csv.writerow({'Título': filme, 'Gênero': genero, 'Duração': duracao})
+
+
+
+
 
 
