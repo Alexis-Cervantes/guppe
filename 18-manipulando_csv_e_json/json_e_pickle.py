2df095fd10d8c772a0c45965b8eda2c5dd371b2d
Alexis Cervantes <iacervantes@outlook.com>
2024-12-18 12:41:48 -0300

    comezando json + pickle jusntos em python

---
+++ 18-manipulando_csv_e_json/json_e_pickle.py
@@ -0,0 +1,37 @@
+"""JSON E PICKLE"""
+'''JSON -> JavaScript Object Notation
+API -> São meios de comunicação entre os serviços oferecidos por empresas (Ex. Twiter, Facebook, Youtube etc) e 
+terceiros(nós desenvolvedores). 
+JSON -> em teoria é um diccionario e trabalha com aspas duplas
+'''
+import json
+
+# Utilzando json de fora basica:
+# ret = json.dumps(['produto', {'PlayStation 4': ('2TB', 'Novo', '220V', '2340')}])
+# print(type(ret))
+# print(ret)
+
+# Utilizando json de forma mais complexa
+# class Gato:
+#     def __init__(self, nome, raca):
+#         self.__nome = nome
+#         self.__raca = raca
+#
+#     @property
+#     def nome(self):
+#         return self.__nome
+#
+#     @property
+#     def raca(self):
+#         return self.__raca
+#
+# felix = Gato('Felix', 'Viralata')
+# print(felix.__dict__)
+# ret = json.dumps(felix.__dict__)
+# print(ret)
+
+# INTEGRANDO JSON COM PICKLE (pip install jsonpickle)
+
+
+
+
