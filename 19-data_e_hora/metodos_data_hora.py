a24d858a18e63c209f1816074abb5efeb8e8e8a2
Alexis Cervantes <iacervantes@outlook.com>
2025-01-12 14:17:31 -0300

    ok

--- 19-data_e_hora/metodos_data_hora.py
+++ 19-data_e_hora/metodos_data_hora.py
@@ -36,24 +36,89 @@ import datetime
 # print(manutecao.weekday())
 
 # Encontrar o data de nascimento
-aniversario = input('Qual sua data de nascimento? dd/mm/yyyy: ')
+# aniversario = input('Qual sua data de nascimento? dd/mm/yyyy: ')
 
-aniversario = aniversario.split('/')
+# aniversario = aniversario.split('/')
 
-aniversario = datetime.datetime(year=int(aniversario[2]), month=int(aniversario[1]), day=int(aniversario[0]))
+# aniversario = datetime.datetime(year=int(aniversario[2]), month=int(aniversario[1]), day=int(aniversario[0]))
 
-if aniversario.weekday() == 0:
-    print('Você nasceu em um lunes')
-elif aniversario.weekday() == 1:
-    print('Você nasceu em um martes')
-elif aniversario.weekday() == 2:
-    print('Você nasceu em um miercoles')
-elif aniversario.weekday() == 3:
-    print('Você nasceu em um jueves')
-elif aniversario.weekday() == 4:
-    print('Você nasceu em um viernes')
-elif aniversario.weekday() == 5:
-    print('Você nasceu em um sabado')
-elif aniversario.weekday() == 6:
-    print('Você nasceu em um domingo')
+# if aniversario.weekday() == 0:
+#     print('Você nasceu em um lunes')
+# elif aniversario.weekday() == 1:
+#     print('Você nasceu em um martes')
+# elif aniversario.weekday() == 2:
+#     print('Você nasceu em um miercoles')
+# elif aniversario.weekday() == 3:
+#     print('Você nasceu em um jueves')
+# elif aniversario.weekday() == 4:
+#     print('Você nasceu em um viernes')
+# elif aniversario.weekday() == 5:
+#     print('Você nasceu em um sabado')
+# elif aniversario.weekday() == 6:
+#     print('Você nasceu em um domingo')
+
+# Formatando datas/horas do padrão americano para o nosso padrão via - strftime() - (string format time)
+# dd/mm/yyyy hora:minuto
+
+# hoje = datetime.datetime.today()
+
+# print(hoje)
+
+# hoje_formatado = hoje.strftime('%d/%m/%Y')
+# hoje_formatado1 = hoje.strftime('%d/%M/%Y')
+# hoje_formatado2 = hoje.strftime('%D/%m/%Y')
+# hoje_formatado3 = hoje.strftime('%d/%B/%Y')
+# hoje_formatado4 = hoje.strftime('%d/%b/%Y')
+# hoje_formatado5 = hoje.strftime('%d de %B de %Y')
+
+# print(hoje_formatado)
+# print(hoje_formatado1)
+# print(hoje_formatado2)
+# print(hoje_formatado3)
+# print(hoje_formatado4)
+# print(hoje_formatado5)
+
+# Criando função que formata os nomes dos meses do ano em portugues
+
+# def formata_data(data):
+#     if data.month == 1:
+#         return f'{data.day} de Enero de {data.year}'
+#     elif data.month == 2:
+#         return f'{data.day} de Febrero de {data.year}'
+#     elif data.month == 3:
+#         return f'{data.day} de Marzo de {data.year}'
+#     elif data.month == 4:
+#         return f'{data.day} de Abril de {data.year}'
+#     elif data.month == 5:
+#         return f'{data.day} de Mayo de {data.year}'
+#     elif data.month == 6:
+#         return f'{data.day} de Junio de {data.year}'
+#     elif data.month == 7:
+#         return f'{data.day} de Julio de {data.year}'
+#     elif data.month == 8:
+#         return f'{data.day} de Agosto de {data.year}'
+#     elif data.month == 9:
+#         return f'{data.day} de Setiembre de {data.year}'
+#     elif data.month == 10:
+#         return f'{data.day} de Octubre de {data.year}'
+#     elif data.month ==11:
+#         return f'{data.day} de Noviembre de {data.year}'
+#     elif data.month == 12:
+#         return f'{data.day} de Diciembre de {data.year}'
+
+# hoje = datetime.datetime.today()
+# print(formata_data(hoje))
+
+
+# Refatorando nosso codigo anterior (instalando biblioteca 'textblob'
+from textblob import TextBlob
+
+def formatando_data(data):
+    return f"{data.day} de {TextBlob(data.strftime('%B')).translate(to='pt-br')} de {data.year}"
+
+hoje = datetime.datetime.today()
+print(formatando_data(hoje))
+
+
+
 
