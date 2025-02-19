1c08ba5c10adba32352765a9723fe26fb1da8a18
Alexis Cervantes <iacervantes@outlook.com>
2025-02-18 21:35:43 -0300

    ok

--- 20-testes/doctest.py
+++ 20-testes/doctest.py
@@ -1,4 +1,28 @@
 """"Doctest"""
+'''
+Doctests são testes que colocamos no 'docstring' das funções/metodos Python.
+Para rodar um test do doctest:
+python -m doctest -v nome_do_módulo.py
+
+- (guppe) D:\codando\ahorapy\PycharmProjects\guppe> python 20-testes/doctest.py
+7
+- (guppe) D:\codando\ahorapy\PycharmProjects\guppe> python -m doctest -v  20-testes/doctest.py
+7
+Trying:
+    soma(1, 2)
+Expecting:
+    3
+ok
+1 items had no tests:
+    doctest
+1 items passed all tests:
+   1 tests in doctest.soma
+1 tests in 2 items.
+1 passed and 0 failed.
+Test passed.
+
+(guppe) D:\codando\ahorapy\PycharmProjects\guppe>
+'''
 
 # Exemplo 1:
 
@@ -9,3 +33,5 @@ def soma(a, b):
     3
     """
     return a + b
+
+print(soma(3, 4)) # 7
